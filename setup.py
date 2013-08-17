764544962d510e1e5ee2eb1d3fefd85530ad9f71
Kunal Parmar <kunalparmar@gmail.com>
2013-08-17 16:30:24 -0700

    Add url to setup.py.

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ def main():
         long_description=open(join(dirname(__file__), 'README.md')).read(),
         author='Kunal Parmar',
         author_email='kunalparmar@gmail.com',
+        url='pypi.python.org/pypi/rotunicode',
         packages=find_packages(exclude=['test']),
         namespace_packages=[b'box'],
         test_suite='test',
