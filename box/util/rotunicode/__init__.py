860ccc62f594d1697d92a836e64244461a2c1b0f
Kunal Parmar <kunalparmar@gmail.com>
2013-12-31 02:13:45 -0800

    Fix build break.

--- box/util/rotunicode/__init__.py
+++ box/util/rotunicode/__init__.py
@@ -3,8 +3,3 @@
 from __future__ import unicode_literals
 from .rotunicode import RotUnicode
 from .utils import ruencode, rudecode
-
-
-__version__ = '0.1.6'
-__author__ = 'Kunal Parmar'
-__email__ = 'kunalparmar@gmail.com'
--- setup.py
+++ setup.py
@@ -4,19 +4,17 @@ from __future__ import unicode_literals
 from setuptools import setup, find_packages
 from os.path import dirname, join
 
-from box.util.rotunicode import __version__, __author__, __email__
-
 
 def main():
     base_dir = dirname(__file__)
     setup(
         name='rotunicode',
-        version=__version__,
+        version='0.1.6',
         description='Python codec for converting between a string of ASCII '
                     'and Unicode chars maintaining readability',
         long_description=open(join(base_dir, 'README.rst')).read(),
-        author=__author__,
-        author_email=__email__,
+        author='Kunal Parmar',
+        author_email='kunalparmar@gmail.com',
         url='https://github.com/box/rotunicode',
         license=open(join(base_dir, 'LICENSE')).read(),
         packages=find_packages(exclude=['test']),
