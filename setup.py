f4ee10b5391ffcfb08c188e523782ec74b0ad3f9
Kunal Parmar <kunalparmar@gmail.com>
2014-04-03 15:29:35 -0700

    Bump version to 1.0.2.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def main():
     base_dir = dirname(__file__)
     setup(
         name='rotunicode',
-        version='1.0.1',
+        version='1.0.2',
         description='Python codec for converting between a string of ASCII '
                     'and Unicode chars maintaining readability',
         long_description=open(join(base_dir, 'README.rst')).read(),
